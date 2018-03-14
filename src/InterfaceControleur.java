b99f6e34730361c5ae35888475910651f226d849
Tagre <mael.clouet@hotmail.fr>
2018-03-14 22:41:14 +0100

    separation de la création du json en 2 étapes

--- src/InterfaceControleur.java
+++ src/InterfaceControleur.java
@@ -56,13 +56,30 @@ public class InterfaceControleur implements Initializable {
 		if(CB_EntryElement.isSelected()){
 			Element entry=new Element(TF_ObjetSource.getText());
 			UtilsSparQL.searchClasses(entry, Integer.parseInt(TF_Classe.getText()));
-			Make_Json_Elem(entry, 0);
+			
+			String part="";
+			part=Make_Json_Elem_Part1(entry, 0);
+			
+			part+="],\n  \"nodes\": [\n";
+			part+="	{\"id\": \""+entry.getName().substring(entry.getName().lastIndexOf("/")+1)+"\", \"type\": "+"\"circle\""+", \"label\": \""+entry.getName()+"\"}";
+			part+=",\n";
+			
 			int cmpt=entry.getClasses().size()+1;
 			if(Integer.parseInt(TF_degre.getText())==2){
 				for(Classe classe:entry.getClasses()){
 					
 				}
 			}
+			
+			
+			part+=Make_Json_Elem_Part2(entry, 0);
+			
+			File file = new File("src/data.json");
+		    BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+		    bw.write(part);
+		    bw.close();
+			
+			
 		} else if (CB_EntryClasse.isSelected()){
 			Classe entry=new Classe(TF_ObjetSource.getText());
 			if(CB_Element.isSelected())
@@ -74,7 +91,21 @@ public class InterfaceControleur implements Initializable {
 			if(CB_EquiClasse.isSelected())
 				UtilsSparQL.searchEquiClasses(entry,  Integer.parseInt(TF_EquiClasse.getText()));
 			
-			Make_Json_Class(entry, 0);
+			String part="";
+			part=Make_Json_Class_Part1(entry, 0);
+			
+			part+="],\n  \"nodes\": [\n";
+			part+="	{\"id\": \""+entry.getName().substring(entry.getName().lastIndexOf("/")+1)+"\", \"type\": "+"\"square\""+", \"label\": \""+entry.getName()+"\"}";
+			part+=",\n";
+			
+			part+=Make_Json_Class_Part2(entry, 0);
+			
+			File file = new File("src/data.json");
+		    BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+		    bw.write(part);
+		    bw.close();
+
+
 			
 		}
 		webEngine.reload();
@@ -112,6 +143,32 @@ public class InterfaceControleur implements Initializable {
 		System.out.println("Json construit");
 	}
 	
+	public String Make_Json_Elem_Part1(Element element, int cmptElmt) throws IOException{
+		String part1="";
+		part1+="{\n  \"graph\": [],\n  \"links\": [\n";
+	    
+	    for(int i=cmptElmt+1;i<element.getClasses().size()+1;i++){
+	    	part1+="	{\"source\": "+cmptElmt+", \"target\": "+i+", \"weight\": \""+"rdf:type"+"\"}";
+	      if(i!=element.getClasses().size())
+	    	  part1+=",\n";
+	    }
+	    return part1;
+	}
+	
+	public String Make_Json_Elem_Part2(Element element, int cmptElmt) throws IOException{
+		String part2="";
+		for(int i=0;i<element.getClasses().size();i++){
+		      //mettre circle ou square selon si label ou non **A FAIRE**
+		    	String name=element.getClasses().get(i).getName();
+		    	part2+="	{\"id\": \""+name.substring(name.lastIndexOf("/")+1)+"\", \"type\": "+"\"square\""+", \"label\": \""+element.getClasses().get(i).getName()+"\"}";
+		    	if(i!=element.getClasses().size()-1){
+		    		part2+=",\n";
+		    	}
+		    }
+		part2+="],\n  \"directed\": true,\n  \"multigraph\": false\n}";
+		return part2;
+	}
+		
 	private void Make_Json_Class(Classe entry, int cmptElmt) throws IOException {
 		File file = new File("src/data.json");
 	    BufferedWriter bw = new BufferedWriter(new FileWriter(file));
@@ -205,6 +262,97 @@ public class InterfaceControleur implements Initializable {
 	    bw.close();
 		System.out.println("Json construit");
 		
+	}
+	
+	private String Make_Json_Class_Part1(Classe entry, int cmptElmt) throws IOException {
+		String part1="";
+		part1+="{\n  \"graph\": [],\n  \"links\": [\n";
+	    
+	    for(int i=cmptElmt+1;i<entry.getElements().size()+1;i++){
+	    	part1+="	{\"source\": "+i+", \"target\": "+cmptElmt+", \"weight\": \""+"rdf:type"+"\"}";
+	      if(i!=entry.getElements().size())
+	    	  part1+=",\n";
+	    }
+	    
+	    int cmpt=entry.getElements().size();
+	    
+	    if(entry.getSurClasses().size()>0 && CB_Element.isSelected())
+	    	part1+=",\n";
+	    for(int i=cmpt+1;i<entry.getSurClasses().size()+1+cmpt;i++){
+	    	part1+="	{\"source\": "+cmptElmt+", \"target\": "+i+", \"weight\": \""+"rdfs:subClassOf"+"\"}";
+		      if(i!=entry.getSurClasses().size()+cmpt)
+		    	  part1+=",\n";
+		    }
+	    
+	    cmpt=cmpt+entry.getSurClasses().size();
+	    
+	    if(entry.getSubClasses().size()>0 && CB_SurClasse.isSelected())
+	    	part1+=",\n";
+	    for(int i=cmpt+1;i<entry.getSubClasses().size()+1+cmpt;i++){
+	    	part1+="	{\"source\": "+i+", \"target\": "+cmptElmt+", \"weight\": \""+"rdfs:subClassOf"+"\"}";
+		    if(i!=entry.getSubClasses().size()+cmpt)
+		    	part1+=",\n";
+		    }
+	    
+	    cmpt=cmpt+entry.getSubClasses().size();
+	    
+	    if(entry.getEquiClasses().size()>0 && CB_EquiClasse.isSelected())
+	    	part1+=",\n";
+	    for(int i=cmpt+1;i<entry.getEquiClasses().size()+1+cmpt;i++){
+	    	part1+="	{\"source\": "+cmptElmt+", \"target\": "+i+", \"weight\": \""+"owl:equivalentClass"+"\"}";
+		      if(i!=entry.getEquiClasses().size()+cmpt)
+		    	  part1+=",\n";
+		    }
+		return part1;
+	}
+	
+	private String Make_Json_Class_Part2(Classe entry, int cmptElmt) throws IOException {
+		String part2="";
+		for(int i=0;i<entry.getElements().size();i++){
+		      //mettre circle ou square selon si label ou non **A FAIRE**
+		    	String name=entry.getElements().get(i).getName();
+		    	part2+="	{\"id\": \""+name.substring(name.lastIndexOf("/")+1)+"\", \"type\": "+"\"circle\""+", \"label\": \""+entry.getElements().get(i).getName()+"\"}";
+		    	if(i!=entry.getElements().size()-1){
+		    		part2+=",\n";
+		    	}
+		    }
+		    
+		    if(entry.getSurClasses().size()>0 && CB_Element.isSelected())
+		    	part2+=",\n";
+		    for(int i=0;i<entry.getSurClasses().size();i++){
+			      //mettre circle ou square selon si label ou non **A FAIRE**
+			    	String name=entry.getSurClasses().get(i).getName();
+			    	part2+="	{\"id\": \""+name.substring(name.lastIndexOf("/")+1)+"\", \"type\": "+"\"square\""+", \"label\": \""+entry.getSurClasses().get(i).getName()+"\"}";
+			    	if(i!=entry.getSurClasses().size()-1){
+			    		part2+=",\n";
+			    	}
+			}
+		    
+		    if(entry.getSubClasses().size()>0 && CB_SurClasse.isSelected())
+		    	part2+=",\n";
+		    for(int i=0;i<entry.getSubClasses().size();i++){
+			      //mettre circle ou square selon si label ou non **A FAIRE**
+			    	String name=entry.getSubClasses().get(i).getName();
+			    	part2+="	{\"id\": \""+name.substring(name.lastIndexOf("/")+1)+"\", \"type\": "+"\"square\""+", \"label\": \""+entry.getSubClasses().get(i).getName()+"\"}";
+			    	if(i!=entry.getSubClasses().size()-1){
+			    		part2+=",\n";
+			    	}
+			}
+		    
+		    if(entry.getEquiClasses().size()>0 && CB_SousClasse.isSelected())
+		    	part2+=",\n";
+		    for(int i=0;i<entry.getEquiClasses().size();i++){
+			      //mettre circle ou square selon si label ou non **A FAIRE**
+			    	String name=entry.getEquiClasses().get(i).getName();
+			    	part2+="	{\"id\": \""+name.substring(name.lastIndexOf("/")+1)+"\", \"type\": "+"\"square\""+", \"label\": \""+entry.getEquiClasses().get(i).getName()+"\"}";
+			    	if(i!=entry.getEquiClasses().size()-1){
+			    		part2+=",\n";
+			    	}
+			}
+		    
+		    
+		    part2+="],\n  \"directed\": true,\n  \"multigraph\": false\n}";
+		    return part2;
 	}
 
 	
